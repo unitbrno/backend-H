543de03b5c21ebbdbb788f3139c6ed5618288443
PatrikH0lop <patrikholop@gmail.com>
2018-04-06 10:58:08 +0200

    Add io module

---
+++ src/core/io.py
@@ -0,0 +1,35 @@
+"""
+Package: core
+Authors: Patrik Holop
+About: Basic function used in project
+Usage examples:
+    1)
+    obj = load_image("../../tests/1/field/0.tif")
+    show_image(obj)
+    2)
+    vector = get_image_vector("../data/obj.tif")
+"""
+
+
+import numpy as np
+from PIL import Image
+
+
+def load_image(filename):
+    """Loads image into memory
+    :return Image object
+    """
+    return Image.open(filename)
+
+
+def get_image_vector(obj, load=False):
+    """Gets number vector from obj"""
+    object = load_image(obj) if load else obj
+    return np.array(object)
+
+
+def show_image(obj):
+    """Shows image"""
+    obj.show()
+
+
